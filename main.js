f9b58ae99c37c3845e07350a0f3e19465562d94c
Natnael Dagnachew <101104482+Nate2784@users.noreply.github.com>
2023-12-24 09:19:06 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+let currentDate = new Date();
+let currentYear = currentDate.getFullYear();
+let currentYearElement = document.getElementById('currentYear')
+let agejokeElement = document.getElementById('ageJoke')
+
+let birthDate = () => prompt('Enter your birth year')
+const calcAge = (birthDate) =>{
+    let age = currentYear - birthDate()
+    return age
+}
+let ageJoke = (age) =>{
+    if(age>18){
+        agejokeElement.innerText = "Dang you old! LMAO";
+        document.getElementById('gif').innerHTML = '<div style="width:100%;height:0;padding-bottom:99%;position:relative;"><iframe src="https://giphy.com/embed/bAjJ1jfbDrgCk" width="100%" height="100%" style="position:absolute;border-radius: 5px;" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div>';
+
+    }else{
+        agejokeElement.innerText = "what's up kiddo :)";
+        document.getElementById('gif').innerHTML = '<div style="width:100%;height:0;padding-bottom:100%;position:relative;"><iframe src="https://giphy.com/embed/13hxeOYjoTWtK8" width="100%" height="100%" style="position:absolute;border-radius: 5px;" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div>';
+    }
+} 
+let getAge = (calcAge,ageJoke) => {
+    var age = calcAge(birthDate);
+    if(age<0){
+        currentYearElement.innerText = `Please enter an appropriate date, And try again.`;
+    }else{
+    currentYearElement.innerText = `You are ${age} Years old.`;
+    ageJoke(age)
+    }
+}
